1c71cac6fd7556c23c14a1077a6e0122988a085d
AlisonJardon <alison.jardon@gmail.com>
2019-11-04 17:27:30 +0100

    first commit

---
+++ app/models/event.rb
@@ -0,0 +1,27 @@
+class Event < ApplicationRecord
+	belongs_to :user 
+	has_many :attendances
+	has_many :users, through: :attendances 
+
+
+	validates :start_date,
+	presence: true
+	
+	validates :duration,
+	presence: true
+	
+	validates :title,
+	presence: true,
+	length: {minimum: 5, maximum: 140, message: 'Le titre doit être compris entre 5 et 140 caractères'}
+	
+	validates :description,
+	presence: true
+	length: {minimum: 20, maximum: 1000, message: 'La description doit faire 20 caractères au minimum et 1000 au maximum'}
+
+	validates :price,
+	presence: true
+
+	validates :location,
+	presence: true
+
+end
